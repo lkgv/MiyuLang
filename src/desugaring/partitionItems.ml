af2b658adbb1430a80d13cbb484df0f6d73dec06
codergwy <codergwy@yahoo.com>
2022-01-26 21:44:30 +0800

    updated partitionItems in desugaring

--- src/desugaring/partitionItems.ml
+++ src/desugaring/partitionItems.ml
@@ -1,8 +1,30 @@
 open Core
-open ParsedAst
+open Utils
+open Parsing.ParsedAst
+
 let partition_items items =
-  let import_defns = List.filter ~f:() items in
-  let class_defns = List.filter ~f:() items in
-  let function_defns = List.filter ~f:() items in
-  let var_defns = List.filter ~f:() items in
-  (import_defns, class_defns, function_defns, var_defns)
+  let rec accu_items items =
+    match items with
+    | ({it= x; _} as item) :: xs -> (
+        let import_defns, class_defns, function_defns, var_defns = accu_items xs in
+        match x with
+        | Import _                   -> ( item :: import_defns
+                                        , class_defns
+                                        , function_defns
+                                        , var_defns )
+        | Class _ | Trait _ | Impl _ ->
+            (import_defns, item :: class_defns, function_defns, var_defns)
+        | Fn _                       -> ( import_defns
+                                        , class_defns
+                                        , item :: function_defns
+                                        , var_defns )
+        | Vars _                     -> ( import_defns
+                                        , class_defns
+                                        , function_defns
+                                        , item :: var_defns )
+        | _                          -> ( import_defns
+                                        , class_defns
+                                        , function_defns
+                                        , var_defns ) )
+    | []                         -> ([], [], [], []) in
+  accu_items items
---
+++ src/desugaring/partitionItems.mli
@@ -0,0 +1,4 @@
+open Utils
+open Parsing.ParsedAst
+
+val partition_items : expr list -> expr list * expr list * expr list * expr list
